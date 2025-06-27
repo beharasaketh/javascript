9411d267595c03225849eb08d858c792a351731e
saketh <140230284+beharasaketh@users.noreply.github.com>
2025-06-27 19:05:39 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,19 @@
+let demo = "vignhesheeeeeee"
+
+console.log(demo.charAt("1"))
+console.log(demo.toUpperCase())
+console.log(demo.split(""))
+console.log(demo.slice(0,7))
+console.log(demo.substring(0,7))
+console.log(demo.substr(0,7))
+console.log(demo.indexOf("e"))
+console.log(demo.lastIndexOf("e"))
+console.log(demo.search(/e/i))
+//for(let i =0; i <=10;i++){
+   // console.log(i.toString().padEnd(4,0))
+   // console.log(i.toString().padStart(4,0))
+console.log(demo.startsWith("v",0))
+console.log(demo.endsWith("i",2))
+console.log(demo.includes("vignhesh"))
+console.log(demo.replace(/e/g,"l",8))
+console.log(demo.length)
